cba91ec4dec658e45fb3bd427eab2a04576f0d2e
razanfawwaz <razanfawwaz1905@gmail.com>
2023-01-12 00:54:38 +0700

    feat: seo and add meta tags

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { DocsThemeConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: <span>Google Developer Student Clubs - USK</span>,
@@ -30,6 +31,56 @@ const config: DocsThemeConfig = {
         </a>
       </p>
     ),
+  },
+  useNextSeoProps() {
+    const { route } = useRouter();
+    return {
+      titleTemplate: "%s – Docs by GDSC USK",
+    };
+  },
+  head: () => {
+    const { asPath } = useRouter();
+    const { frontMatter } = useConfig();
+    return (
+      <>
+        <meta property="og:url" content={`https://docs.gdscusk.com${asPath}`} />
+        <meta
+          property="og:title"
+          content={frontMatter.title || "Docs by GDSC USK"}
+        />
+        <meta
+          property="og:description"
+          content={
+            frontMatter.description ||
+            "Help Students #GrowTogether by providing accessible teaching material. By Google Developer Student Clubs - Universitas Syiah Kuala"
+          }
+        />
+        <meta
+          property="og:title"
+          content={frontMatter.title || "Docs by GDSC USK"}
+        />
+        <meta
+          property="og:description"
+          content="Help Students #GrowTogether by providing accessible teaching material. By Google Developer Student Clubs - Universitas Syiah Kuala"
+        />
+        <meta property="og:image" content="/meta.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="docs.gdscusk.com" />
+        <meta
+          property="twitter:url"
+          content={`https://docs.gdscusk.com${asPath}`}
+        />
+        <meta
+          name="twitter:title"
+          content={frontMatter.title || "Docs by GDSC USK"}
+        />
+        <meta
+          name="twitter:description"
+          content="Help Students #GrowTogether by providing accessible teaching material. By Google Developer Student Clubs - Universitas Syiah Kuala"
+        />
+        <meta name="twitter:image" content="/meta.jpg" />
+      </>
+    );
   },
 };
 
